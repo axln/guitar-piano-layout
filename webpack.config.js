c4a61b806adcfe12e93e1287fd5fba6a3abb3f18
Alexey Nesterenko <alexei.ne@gmail.com>
2020-03-15 23:47:34 +0700

    entry point file renamed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path              = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
