897fcee4b460a5d33812118a3d5525772c73a280
Alexey Nesterenko <alexei.ne@gmail.com>
2020-03-17 03:33:00 +0700

    redux debug disabled

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 import * as reducers from './reducer';
 
-export const store = createStore(combineReducers(reducers),
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+export const store = createStore(combineReducers(reducers)
+    //, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
