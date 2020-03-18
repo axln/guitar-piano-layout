344a9bb42298764a7bf86f750a15fbfd55dc1bbc
Alexey Nesterenko <alexei.ne@gmail.com>
2020-03-19 04:08:03 +0700

    update condition fixed

--- src/component/Piano/PianoKey.js
+++ src/component/Piano/PianoKey.js
@@ -3,10 +3,11 @@ import { stopNote, playNote } from '../../lib/SoundGenerator';
 
 export class PianoKey extends Component {
     shouldComponentUpdate(nextProps) {
-        const { pushed, baseKey, index } = this.props;
+        const { pushed, baseKey, keyOffset, index } = this.props;
         return pushed !== nextProps.pushed ||
             baseKey !== nextProps.baseKey ||
-            index !== nextProps.index;
+            index !== nextProps.index ||
+            keyOffset !== nextProps.keyOffset;
     }
 
     handleMouseDown = () => {
